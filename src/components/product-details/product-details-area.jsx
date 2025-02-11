cd79f0cb8a54d853b748240472602fbc9d8e6a62
fastestsunil <sunil141292@gmail.com>
2025-02-11 14:37:36 +0530

    Refactor product details component: improve code formatting and simplify rendering
    - Update import statements with consistent single quotes
    - Simplify component rendering with inline fragment
    - Improve prop destructuring and default value formatting
    - Remove unnecessary whitespace and optimize code structure

--- src/components/product-details/product-details-area.jsx
+++ src/components/product-details/product-details-area.jsx
@@ -1,12 +1,12 @@
 'use client';
-import React from "react";
-import PrdDetailsLoader from "../loader/prd-details-loader";
-import ErrorMsg from "../common/error-msg";
-import ProductDetailsBreadcrumb from "../breadcrumb/product-details-breadcrumb";
-import { useGetProductQuery } from "@/redux/features/productApi";
-import ProductDetailsContent from "./product-details-content";
+import React from 'react';
+import PrdDetailsLoader from '../loader/prd-details-loader';
+import ErrorMsg from '../common/error-msg';
+import ProductDetailsBreadcrumb from '../breadcrumb/product-details-breadcrumb';
+import { useGetProductQuery } from '@/redux/features/productApi';
+import ProductDetailsContent from './product-details-content';
 
-const ProductDetailsArea = ({id="6431364df5a812bd37e765ac"}) => {
+const ProductDetailsArea = ({ id = '6431364df5a812bd37e765ac' }) => {
   const { data: product, isLoading, isError } = useGetProductQuery(id);
   // decide what to render
   let content = null;
@@ -19,16 +19,15 @@ const ProductDetailsArea = ({id="6431364df5a812bd37e765ac"}) => {
   if (!isLoading && !isError && product) {
     content = (
       <>
-        <ProductDetailsBreadcrumb category={product.category.name} title={product.title} />
+        <ProductDetailsBreadcrumb
+          category={product.category.name}
+          title={product.title}
+        />
         <ProductDetailsContent productItem={product} />
       </>
     );
   }
-  return (
-    <>
-      {content}
-    </>
-  );
+  return <>{content}</>;
 };
 
 export default ProductDetailsArea;
