d9b554e73ffd279b2bffcb340a7354327a1bc2ce
fastestsunil <sunil141292@gmail.com>
2025-04-16 12:58:14 +0530

    Add HeroBanner component to HomePage for enhanced visual appeal

---
+++ src/components/V2/heroBanner/HeroBanner.jsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { CldImage } from 'next-cloudinary';
+
+export default function HeroBanner() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        maxWidth: '1680px',
+        margin: '0 auto',
+        padding: '0 0.75rem',
+      }}
+    >
+      <CldImage
+        src="banner1_zyqptl"
+        alt="Banner"
+        width={1680}
+        height={420}
+        sizes="100vw"
+        preserveTransformations={true}
+        crop="limit"
+        priority
+        style={{
+          width: '100%',
+          height: 'auto',
+          borderRadius: '8px',
+          marginBottom: '1rem',
+          marginTop: '1rem',
+        }}
+      />
+    </div>
+  );
+}
