9481f598909029acec808e75122618973cd7f969
fastestsunil <sunil141292@gmail.com>
2025-04-23 12:42:59 +0530

    Update image source and enforce padding in BannerWithDiscount component for enhanced visual clarity and layout consistency

--- src/components/V2/heroBanner/BannerWithDiscount.jsx
+++ src/components/V2/heroBanner/BannerWithDiscount.jsx
@@ -7,7 +7,7 @@ export default function BannerWithDiscount() {
   return (
     <div className={styles.bannerContainer}>
       <CldImage
-        src="EWO_10_ylw3co"
+        src="SAVE_10_ON_YOUR_FIRST_ORDER_IF_YOU_SIGN_UP_TODAY_JOIN_NOW_bgujkc"
         width={1680}
         height={300}
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 1680px"
