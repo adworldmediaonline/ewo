5d31e6e25088bf33d723666097f356d1ce2e3da5
fastestsunil <sunil141292@gmail.com>
2025-07-03 13:27:27 +0530

    footer ussi on the coupn page rewsovle

--- src/app/coupon/layout.jsx
+++ src/app/coupon/layout.jsx
@@ -1,5 +1,6 @@
 import ShopBreadcrumb from '../../components/breadcrumb/shop-breadcrumb';
-import Footer from '../../layout/footers/Footer';
+import Footer from '../../layout/footers/footer';
+
 import HeaderV2 from '../../layout/headers/HeaderV2';
 import Wrapper from '../../layout/wrapper';
 
@@ -9,7 +10,7 @@ export default function CouponLayout({ children }) {
       <HeaderV2 />
       <ShopBreadcrumb subtitle="Coupons" />
       {children}
-      <Footer primary_style={true} />
+      <Footer style_2={true} />
     </Wrapper>
   );
 }
