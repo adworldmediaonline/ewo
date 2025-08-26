e518b9bdf8ccbd59b36746b356fcee13dc9760b2
fastestsunil <sunil141292@gmail.com>
2025-08-26 09:22:41 +0530

    refactor(workspace & profile): remove unused profile page and update workspace configuration
    - Deleted the `page.tsx` component from the profile directory to eliminate unused functionality and streamline the codebase.
    - Updated the workspace configuration by removing the reference to the `ewo-admin` directory, ensuring a cleaner workspace structure.

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -2,32 +2,24 @@ import {
   emailOTPClient,
   inferAdditionalFields,
 } from 'better-auth/client/plugins';
-import { nextCookies } from 'better-auth/next-js';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
+  // baseURL: 'http://localhost:8090/api/auth',
+  baseURL: 'https://ewo-backend.vercel.app/api/auth',
   plugins: [
     emailOTPClient(),
     inferAdditionalFields({
       user: {
         role: {
-          defaultValue: 'user',
+          // defaultValue: 'user',
           type: 'string',
-          input: false,
-          required: true,
-          // enum: ['super_admin', 'admin', 'user'],
+          // input: false,
+          required: false,
         },
       },
     }),
-    nextCookies(),
   ],
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_BACKEND_URL_LOCAL
-      : process.env.NEXT_PUBLIC_BACKEND_URL_PROD,
-  fetchOptions: {
-    credentials: 'include',
-  },
 });
 
 export type ClientSession = typeof authClient.$Infer.Session;
