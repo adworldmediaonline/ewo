80574d72055e1f4ad176d38d6e2d710607cdb3b8
fastestsunil <sunil141292@gmail.com>
2025-09-02 14:02:01 +0530

    fix(page): update banner IDs for Labor Day sale promotion
    - Changed the `desktopPublicId` and `mobilePublicId` in the `Banner` component to reflect the new promotional campaign for Labor Day, ensuring accurate display of the sale information.
    - This update aims to enhance user engagement by promoting the current sale effectively on the homepage.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,8 +11,8 @@ export default async function HomePage() {
     <>
       <HeroBanner />
       <Banner
-        desktopPublicId="Banner_Post_zytdpx"
-        mobilePublicId="Banner_Post_zytdpx"
+        desktopPublicId="Extended_LABOR_DAY_SALE_20_OFF_On_All_Off-Road_Parts_txzlas"
+        mobilePublicId="Extended_LABOR_DAY_SALE_20_OFF_On_All_Off-Road_Parts_txzlas"
         priority={false}
         showButton={false}
         aspectRatio={3.69} // 1920/520 ≈ 3.69 for 520px height
