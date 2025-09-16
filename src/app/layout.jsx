420fbc8d581040638c5b8e091580a84f33e735fb
fastestsunil <sunil141292@gmail.com>
2025-09-16 10:21:54 +0530

    refactor(layout): simplify Toaster component and remove unused favicon
    - Updated the `RootLayout` component to utilize the `Toaster` with rich colors, enhancing the visual feedback for notifications.
    - Removed the unused `favicon.ico` file to clean up the project structure and reduce unnecessary assets.
    - Modified the `robots.js` file to remove commented-out paths, improving code clarity and maintainability.
    - These changes aim to streamline the layout component and optimize project organization.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -88,16 +88,7 @@ export default function RootLayout({ children }) {
           </Wrapper>
         </Providers>
 
-        <Toaster
-          // position="top-center"
-          toastOptions={{
-            error: 'bg-red-500 text-white',
-            success: 'bg-green-500 text-white',
-            warning: 'bg-yellow-500 text-white',
-            info: 'bg-blue-500 text-white',
-            default: 'bg-gray-500 text-white',
-          }}
-        />
+        <Toaster richColors />
 
         {/* Tawk.to Script - Simple approach compatible with server components */}
 
