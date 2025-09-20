c6f36c0ccc67053e21d44f932546ba05c3e82d47
fastestsunil <sunil141292@gmail.com>
2025-09-20 12:54:21 +0530

    chore(contact): comment out phone-related UI elements across multiple components
    - Commented out phone number display and related UI elements in `ReturnsPage`, `TermsPage`, `ThankYouPage`, `OffCanvas`, `ContactArea`, `ContactForm`, and `FooterTwo` components.
    - These changes aim to temporarily disable phone contact information while maintaining the code for potential future use, improving the clarity of the components without removing the functionality entirely.

--- src/components/common/off-canvas.jsx
+++ src/components/common/off-canvas.jsx
@@ -83,11 +83,11 @@ const OffCanvas = ({
                   <Image src={contact_img} alt="contact_img" />
                 </span>
               </div>
-              <div className="offcanvas__contact-content">
+              {/* <div className="offcanvas__contact-content">
                 <h3 className="offcanvas__contact-title">
                   <a href="tel:1-866-396-7623">1-866-EWO-ROAD (396-7623)</a>
                 </h3>
-              </div>
+              </div> */}
             </div>
             <div className="offcanvas__btn">
               <Link href="/contact" className="tp-btn-2 tp-btn-border-2">
