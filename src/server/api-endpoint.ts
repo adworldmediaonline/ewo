3fc6b39f52555db7c10001db98aad88481b493d7
fastestsunil <sunil141292@gmail.com>
2025-11-04 04:41:28 +0530

    feat: update HeroBanner component to fetch and display active banners
    - Refactored HeroBanner to asynchronously retrieve active banners from the server.
    - Updated rendering logic to display banner images and content dynamically based on fetched data.
    - Added fallback content for cases when no banners are available, enhancing user experience.
    - Introduced a new API endpoint for fetching active banners, improving data management.

--- src/server/api-endpoint.ts
+++ src/server/api-endpoint.ts
@@ -2,4 +2,5 @@ export const API_ENDPOINT = {
   CATEGORIES: '/api/category/show',
   PRODUCTS_SHOW: '/api/product/show',
   PRODUCTS_SINGLE: '/api/product/single-product',
+  BANNER: '/api/banner/active',
 };
